b52f1469a8ebe22fc2f3938fd47ab9bd98348eb3
Ana-Vartasashvili <anavartasashvili30@gmail.com>
2024-02-28 21:12:03 +0400

    add main card component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,10 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import MainCard from '@/components/UI/MainCard.vue'
 </script>
 
 <template>
-  <div class="relative">
-    <img src="./assets/images/heroImage.jpg" alt="earth" class="w-full h-72 object-cover" />
-    <img
-      src="./assets/icons/wrLogo.svg"
-      alt="world ranks logo"
-      class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
-    />
-  </div>
-
-  <RouterView class="absolute top-56 left-1/2 -translate-x-1/2 font-vietnam-pro" />
+  <MainCard>
+    <RouterView class="absolute top-56 left-1/2 -translate-x-1/2 font-vietnam-pro" />
+  </MainCard>
 </template>
---
+++ src/components/UI/MainCard.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div class="relative">
+      <img src="../../assets/images/heroImage.jpg" alt="earth" class="w-full h-72 object-cover" />
+      <img
+        src="../../assets/icons/wrLogo.svg"
+        alt="world ranks logo"
+        class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+      />
+    </div>
+
+    <slot />
+  </div>
+</template>
